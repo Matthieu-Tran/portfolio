69568a1b086407d3c6a3ff4a4cf2973182618f0d
Matthieu <matthieu.tran@utt.fr>
2023-01-30 21:34:29 +0100

    changing github-username

--- src/data/projects-components.js
+++ src/data/projects-components.js
@@ -30,7 +30,7 @@ export const projects = [
       languages: "Javascript / Node",
       date: "OCT-2022 / JAN-2023",
       moreInfo: "Led a team of 4 members for a project that parses .gift files and creates a bank of questions. This project involved conception with creation of scope statements, coding and testing. ",
-      link: "https://github.com/Tabasqueau/questions-generator",
+      link: "https://github.com/Matthieu-Tran/questions-generator",
     },
     {
       title: "Food Blog",
@@ -40,7 +40,7 @@ export const projects = [
       languages: "HTML / CSS / PHP / MySQL/ Bootstrap",
       date: "OCT-2021 / DEC-2021",
       moreInfo: "This project that I built with a pair is recreating a Food-Blog where users can post food recipe. They have to sign-up / sign-in in order to create / comment / rate the others members recipe. There is also a backend with PHP with a database that we created. The admin of the blog can add / remove moderator from the website.",
-      link: "https://github.com/Tabasqueau/Selling-Website",
+      link: "https://github.com/Matthieu-Tran/Food-Blog",
     },
     {
       title: "Travelling salesman problem",
@@ -50,7 +50,7 @@ export const projects = [
       languages: "Python",
       date: "MAY 2022",
       moreInfo: "Program in Python that imitates the travelling salesman problem but with legos. To make short, the Traveling Salesman Problem (TSP) seeks the shortest route visiting cities and returning to start. It is NP-hard, so approximations are used. It has practical applications in various fields. In this program it has to find the best lego tower with 4 different creteria: the diversity of legos, the tower with the most red legos, the heighest tower and the smallest one",
-      link: "https://github.com/Tabasqueau/Travelling-salesman-problem",
+      link: "https://github.com/Matthieu-Tran/Travelling-salesman-problem",
     },
     {
       title: "RPG",
@@ -60,7 +60,7 @@ export const projects = [
       languages: "Java",
       date: "OCT-2021 / NOV-2021",
       moreInfo: "Small Role Play Game in a command line written in Java. It features the use of nested enums, fields, constructors, getters, setters, random number generation, error checking, and the implementation of the hashCode, equals and toString methods. It also includes unit testing",
-      link: "https://github.com/Tabasqueau/rpg-command-line",
+      link: "https://github.com/Matthieu-Tran/rpg-command-line",
     },
     {
       title: "E-Commerce Website",
@@ -70,7 +70,7 @@ export const projects = [
       languages: "HTML / CSS / JavaScript",
       date: "OCT-2020",
       moreInfo: "Created a front-end web application using HTML and CSS to describe an imaginary company by leveraging JavaScript map integration.",
-      link: "https://github.com/Tabasqueau/AirJar",
+      link: "https://github.com/Matthieu-Tran/AirJar",
     },
     {
       title: "Discord Bot",
@@ -80,7 +80,7 @@ export const projects = [
       languages: "Javascript",
       date: "March 2021",
       moreInfo: "Developed a discord bot that recreated the card game, The Werewolves of Millers Hollow, that handled the basic game commands.",
-      link: "https://github.com/Tabasqueau/discord-bot-werewolves",
+      link: "https://github.com/Matthieu-Tran/discord-bot-werewolves",
     },
 
   ];
